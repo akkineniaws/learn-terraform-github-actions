6626f3c374a5268b9aa3eb5812a9ef17637b2300
Amarnath Akkineni <a2az@pge.com>
2023-03-19 19:08:52 -0700

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "A-Terraform-Demos"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "a-demo-github-actions"
     }
   }
 }
